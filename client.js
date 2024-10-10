86d8cdc18ac963e4a8125e18f9d10822788afddf
andrastoth021 <andrastoth021@gmail.com>
2024-10-10 19:21:05 +0200

    implement fetch on client side for getting every todo

--- client.js
+++ client.js
@@ -1,3 +1,14 @@
 const prompt = require('prompt-sync')()
 
 const BASE_URL = "http://localhost:3000";
+
+// GET REQUEST
+fetch(`${BASE_URL}/todos`, { method: 'GET' })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error(error)
+    })
+
