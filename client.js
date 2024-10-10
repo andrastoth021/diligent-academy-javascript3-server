095c3c17b612305e3686578852fbb182d51ce346
andrastoth021 <andrastoth021@gmail.com>
2024-10-10 18:41:23 +0200

    tasks written together

---
+++ client.js
@@ -0,0 +1,56 @@
+const prompt = require('prompt-sync')()
+
+const BASE_URL = "http://localhost:3000";
+
+/*fetch(`${BASE_URL}/users?name=${'john'}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error(error)
+    })
+*/
+
+/*fetch(`${BASE_URL}/users`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name: "Bob Marley"})
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error(error)
+    })*/
+
+/*fetch(`${BASE_URL}/users/${1}`, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name: "Bob Marley"})
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error(error)
+    })*/
+
+
+/*fetch(`${BASE_URL}/users/${1}`, {
+    method: 'DELETE'
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error(error)
+    })
+*/
