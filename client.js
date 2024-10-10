9e989798d38783dde706f8d48724dcee72a32aa6
kabosnn <anagynandor@gmail.com>
2024-10-10 19:48:48 +0200

    add post operation

--- client.js
+++ client.js
@@ -1,3 +1,21 @@
 const prompt = require('prompt-sync')()
 
 const BASE_URL = "http://localhost:3000";
+
+const taskToDo = prompt()
+
+fetch('http://localhost:3000/todos', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({task: taskToDo }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("To-do added:", data);
+    })
+    .catch((error) => {
+      console.error("Error adding to-do:", error);
+    });
+  
